c130f205845cac5111c7ff64e107b5d661932442
Agney Menon <Agney Menon>
2016-09-30 22:24:38 +0530

    Products Page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     console.log("Welcome");
+    $('.carousel').carousel();
+    $('#tabs a').click(function (e) {
+	  e.preventDefault()
+	  $(this).tab('show')
+	});
 });
